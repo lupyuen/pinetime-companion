078e6979341917a0f8247b96d8126bae867e7dee
lupyuen <luppy@appkaki.com>
2020-06-19 13:26:08 +0800

    Added bloc

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Refer to the articles...
 1. [_"Convert Go to Flutter and Dart for PineTime Companion App"_](https://lupyuen.github.io/pinetime-rust-mynewt/articles/companion)
 
 1. [_"Your First Bluetooth Low Energy App with Flutter"_](https://lupyuen.github.io/pinetime-rust-mynewt/articles/flutter)
+
+Based on https://github.com/felangel/Bloc/tree/master/examples/flutter_weather
--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,67 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:http/http.dart' as http;
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'simple_bloc_delegate.dart';
+import 'widgets/widgets.dart';
+import 'repositories/repositories.dart';
+import 'blocs/blocs.dart';
 import 'widgets.dart';
 import 'newtmgr.dart';
 
 void main() {
+  final WeatherRepository weatherRepository = WeatherRepository(
+    weatherApiClient: WeatherApiClient(
+      httpClient: http.Client(),
+    ),
+  );
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeBloc>(
+          create: (context) => ThemeBloc(),
+        ),
+        BlocProvider<SettingsBloc>(
+          create: (context) => SettingsBloc(),
+        ),
+      ],
+      child: App(weatherRepository: weatherRepository),
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  final WeatherRepository weatherRepository;
+
+  App({Key key, @required this.weatherRepository})
+      : assert(weatherRepository != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ThemeBloc, ThemeState>(
+      builder: (context, themeState) {
+        return MaterialApp(
+          title: 'Flutter Weather',
+          theme: themeState.theme,
+          home: BlocProvider(
+            create: (context) => WeatherBloc(
+              weatherRepository: weatherRepository,
+            ),
+            child: Weather(),
+          ),
+        );
+      },
+    );
+  }
+}
+
+void oldMain() {
   runApp(FlutterBlueApp());
 }
 
